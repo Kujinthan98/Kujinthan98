4196153cb8d78db53cee11366885db7d9350226c
Kujinthan <126468021+Kujinthan98@users.noreply.github.com>
2023-12-01 21:15:10 +0530

    grid finished

---
+++ react-fruitstore/src/App.js
@@ -0,0 +1,15 @@
+import Appbar from "./components/Appbar";
+import Items from "./components/Items";
+import Filters from "./components/filters";
+
+function App() {
+  return (
+    <>
+    <Appbar />
+    <Filters/>
+    <Items/>
+    </>
+    )
+}
+
+export default App;
---
+++ react-fruitstore/src/components/FruitsCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const FruitsCard = ({ fruit }) => {
+  return (
+    <div className="rounded-2xl overflow-hidden shadow-xl transform hover:scale-110 duration-100">
+      <img
+        src={require(`../asserts/${fruit.image}.jpeg`)}
+        alt={`${fruit.image}`}
+        className="w-full h-52 object-cover"
+      />
+      <div className="flex flex-col items-center my-2 py-2">
+        <span className="font-body text-slate-500 block">{fruit.name}</span>
+        <span className="font-body text-slate-500 block">₹{fruit.price}</span>
+      </div>
+    </div>
+  );
+};
+
+export default FruitsCard;
